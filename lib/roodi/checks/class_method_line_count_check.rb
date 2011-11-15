2da3fe5cf851a2bd62f29e5e380cff6b56a3144f
Cam Smith <csmith@dius.com.au>
2011-11-15 14:16:28 +1100

    ADded class method line count check

---
+++ lib/roodi/checks/class_method_line_count_check.rb
@@ -0,0 +1,19 @@
+require 'roodi/checks/line_count_check'
+
+module Roodi
+  module Checks
+    # Checks a method to make sure the number of lines it has is under the specified limit.
+    #
+    # A method getting too large is a code smell that indicates it might be doing more than one
+    # thing and becoming hard to test.  It should probably be refactored into multiple methods
+    # that each do a single thing well.
+    class ClassMethodLineCountCheck < LineCountCheck
+      DEFAULT_LINE_COUNT = 20
+
+      def initialize(options = {})
+        line_count = options['line_count'] || DEFAULT_LINE_COUNT
+        super([:defs], line_count, 'Class Method')
+      end
+    end
+  end
+end
